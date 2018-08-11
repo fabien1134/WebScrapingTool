fdba698e52b2f5a957966466488d29eb1933d09c
Fabien Bibana-Bessala <fabien1134@hotmail.co.uk>
2018-08-11 10:51:24 +0100

    Add project files.

---
+++ WebScrapingTool/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrapingTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool applicationContinue = true;
+            string input = string.Join(" ", args);
+
+            while (applicationContinue)
+            {
+                //Select input from either arguments or input
+                input = Console.ReadLine();
+
+                //Validate Input
+
+
+
+
+            }
+
+            Console.WriteLine($"End Of Execution: {DateTime.Now}");
+        }
+    }
+}
