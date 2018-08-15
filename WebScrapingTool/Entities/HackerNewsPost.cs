396a1cf2dc4127279b919d83f390f39c726a5791
Fabien Bibana-Bessala <fabien1134@hotmail.co.uk>
2018-08-15 07:23:14 +0100

    Adding Separation of the .Net property name and post serialization expected json property name.

--- WebScrapingTool/Entities/HackerNewsPost.cs
+++ WebScrapingTool/Entities/HackerNewsPost.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +8,18 @@ using System.Threading.Tasks;
 namespace WebScrapingTool.Entities
 {   //Will be used to store each valid post in a collection the serialize to JSON
     public class HackerNewsPost
-    {
+    {   //The json property name will appear post json serialization regardless of the class property name
+        [JsonProperty(PropertyName = "title")]
         public string title { get; set; }
+        [JsonProperty(PropertyName = "uri")]
         public string uri { get; set; }
+        [JsonProperty(PropertyName = "author")]
         public string author { get; set; }
+        [JsonProperty(PropertyName = "points")]
         public int points { get; set; }
+        [JsonProperty(PropertyName = "comments")]
         public int comments { get; set; }
+        [JsonProperty(PropertyName = "rank")]
         public int rank { get; set; }
     }
 }
